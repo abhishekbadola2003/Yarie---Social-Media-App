3e14781d6545c3abeb1925f5be5725684b3f1d2b
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-11-26 17:36:04 +0530

    Learnings in Typescript
    Get to know about new things in typescript

---
+++ main.js
@@ -0,0 +1,10 @@
+var fname = "abhi";
+console.log(fname);
+function sum(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+console.log(sum(3, 5));
+console.log(sub(5, 3));
--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-let fname:string = abhi;
+type person = {
+    name: string;
+    id: number;
+    employee: boolean;
+    class:string
+}
+
+const person1:person={
+    name: "abhi",
+    id:1,
+    employee:true,
+    class:"senior"
+}
+
+const next:person={
+    name:"abhi",
+    employee:true,
+    id:2,
+    class:"junior"
+}
