4aca17cce8a2031a083e0ccfe898286f7c0cdbea
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-11-27 15:06:22 +0530

    Learnings in Typescript

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
-type person = {
-    num1:number,
-    num2:number,
+import * as dotenv from 'dotenv'
+dotenv.config();
 
+import express from 'express'
+import { json,urlencoded } from 'body-parser';
+import mongoose from 'mongoose';
+
+const app = express()
+
+app.use(urlencoded({
+    extended: true
+}))
+app.use(json())
+
+
+const start =async()=>{
+    if (!process.env.MONGO_URI) throw new Error('MONGO_URI is required!')
+        try {
+           await mongoose.connect(process.env.MONGO_URI)
+        } catch (err) {
+            throw new Error('database error!')
+        }
+ 
+
+app.listen(7000, ()=>{
+    console.log("Server has started running on port 7000");
+    
+})
 }
 
-const first:person={
-    num1:5,
-    num2:4,
-}
-
-
-const mul=(first:person)=>{
-    return first.num1*first.num2;
-     
-}
-console.log(mul(first));
+start()
