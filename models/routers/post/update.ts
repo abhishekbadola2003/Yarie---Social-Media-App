0ebf0f5f4a4b4494be0ebfedbefd0396bc3586b4
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-11-29 11:26:19 +0530

    Learnings in Typescript

---
+++ models/routers/post/update.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Router } from "express";
+
+const router = Router();
+
+router.post(
+  "/api/post/update/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+  }
+);
