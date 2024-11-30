25ab0623e7d7ac7ae3c58d27cf25687cf4c020f1
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-11-30 18:08:15 +0530

    Update new.ts

--- src/routers/comment/new.ts
+++ src/routers/comment/new.ts
@@ -5,10 +5,24 @@ import { error } from "console";
 const router = Router();
 
 router.post(
-  "./api/models/routers/comment/new/:id",
+  "/api/comment/new",
   async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { NewComment, Content } = req.body;
-    } catch (error) {}
+    const { userName, Content } = req.body;
+
+    if (!Content) {
+      const Error = new error("no content written") as CustomError;
+      error.status = 400;
+      return next(error);
+    }
+
+    const NewComment = new Comment({
+      userName: userName ? userName : "Anonymous",
+    });
+
+    await NewComment.save();
+
+    res.status(201).send(NewComment);
   }
 );
+
+export { router as NewCommentRouter };
