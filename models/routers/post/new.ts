eb2ef0e9cd38c44d9007142a1205afa7eb247200
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-11-30 13:06:11 +0530

    Learnings in Typescript and express

---
+++ models/routers/post/new.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { Response, Request, NextFunction, ErrorRequestHandler } from "express";
+import Post from "./post";
+import { error } from "console";
+const router = Router();
+
+router.get(
+  "./api/post/show/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    if (!id) {
+      const error = new Error("post id is required!") as CustomError;
+    }
+
+    try {
+      await Post.findOneAndDelete({ _id: id });
+    } catch (err) {
+      next(new Error("Post cannot be updated."));
+    }
+
+    res.status(200).json({ success: true });
+  }
+);
+export { router as showPostRouter };
