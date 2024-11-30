68819da54ad9a56e857c8ab8ab5570345448c0bf
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-11-30 15:30:04 +0530

    Learnings in Typescript and Express.js

--- models/routers/post/delete.ts
+++ models/routers/post/delete.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { Response, Request, NextFunction, ErrorRequestHandler } from "express";
-import Post from "./routers/post/post";
+import Post from "./post";
+import { error } from "console";
+
 const router = Router();
 
 router.delete(
@@ -13,8 +15,12 @@ router.delete(
     }
 
     try {
-      await Post.findOneAndRemove({ _id: id });
-    } catch (err) {}
+      await Post.findOneAndDelete({ _id: id });
+    } catch (err) {
+      next(new Error("Post cannot be updated."));
+    }
+
+    res.status(200).json({ success: true });
   }
 );
 export { router as deletePostRouter };
