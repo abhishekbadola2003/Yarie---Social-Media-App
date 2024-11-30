70dd1893eefbe9b43449384e61dcb5fcf4df8461
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-11-30 18:00:19 +0530

    Learnings in Express

--- src/routers/comment/new.ts
+++ src/routers/comment/new.ts
@@ -5,27 +5,10 @@ import { error } from "console";
 const router = Router();
 
 router.post(
-  "./api/post/comment/:id",
+  "./api/models/routers/comment/new/:id",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { userName, content } = req.body;
-
-      if (!content) {
-        const error = new Error("content is required!") as CustomError;
-        error.status = 400;
-        return next(error);
-      }
-
-      const newComment = new Comment({
-        userName: userName ? userName : "anonymous",
-      });
-
-      await newComment.save();
-    } catch (err) {
-      next(new Error("Post cannot be showed."));
-    }
-
-    res.status(200).json({ success: true });
+      const { NewComment, Content } = req.body;
+    } catch (error) {}
   }
 );
-export { router as NewCommentRouter };
