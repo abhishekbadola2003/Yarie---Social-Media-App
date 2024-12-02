cf09a41ae7554e2755531332c4a8138263596e8f
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-02 11:02:32 +0530

    Update new.ts

--- src/routers/comment/new.ts
+++ src/routers/comment/new.ts
@@ -2,16 +2,17 @@ import { Router } from "express";
 import { Response, Request, NextFunction, ErrorRequestHandler } from "express";
 import Comment from "../../models/comment";
 import { error } from "console";
+import Post from "../../models/post";
 const router = Router();
 
 router.post(
   "/api/comment/new",
   async (req: Request, res: Response, next: NextFunction) => {
     const { userName, Content } = req.body;
+    const postId = req.params;
 
     if (!Content) {
-      const Error = new error("no content written") as CustomError;
-      error.status = 400;
+      const Error = new error("no content written", 400) as CustomError;
       return next(error);
     }
 
@@ -21,7 +22,13 @@ router.post(
 
     await NewComment.save();
 
-    res.status(201).send(NewComment);
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: postId },
+      { $push: { comments: NewComment } },
+      { new: true }
+    );
+
+    res.status(201).send(updatedPost);
   }
 );
 
