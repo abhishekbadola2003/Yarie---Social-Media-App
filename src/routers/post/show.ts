f72c45f4c5a354b5e704c44cca5ce68812c85789
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-02 16:42:37 +0530

    Update show.ts

--- src/routers/post/show.ts
+++ src/routers/post/show.ts
@@ -2,18 +2,27 @@ import { Router, Request, Response, NextFunction } from "express";
 import Post from "../../models/post";
 
 const router = Router();
+
 router.get(
   "/api/post/show/:id",
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { id } = req.params;
 
     if (!id) {
       const allPosts = await Post.find();
-      return res.status(200).send(allPosts);
+      res.status(200).json(allPosts);
+      return;
     }
+
     const post = await Post.findOne({ _id: id });
 
-    res.status(200).send(post);
+    if (!post) {
+      res.status(404).json({ message: "Post not found" });
+      return;
+    }
+
+    res.status(200).json(post);
   }
 );
+
 export { router as showPostRouter };
