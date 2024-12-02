aa114f0b7f1aa77b0c0594374c098d25a560df2c
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-02 12:20:29 +0530

    Learnings in Typescript using Express.js

--- main.ts
+++ main.ts
@@ -133,3 +133,26 @@ const start = async () => {
 // };
 
 // start();
+
+
+
+import { Express, RequestHandler } from "express"
+const router = Router();
+
+router.post(
+    "/api/comment/:commentid/delete/:postId", async (req: Request, res: Response, next: NextFunction) => {
+        const { postId, commentid } = req.params;
+
+        if (!commentid || !postId) {
+            const error = new Error("commentid or postid is missing") as CustomError
+            error.status = 400
+            return next(error)
+        }
+
+        try {
+            await commentid.findOneAndDelete(_id: postId)
+        } catch (err) {
+            next(error("Comment cannot be updated."))
+        }
+        
+})
--- src/routers/comment/delete.ts
+++ src/routers/comment/delete.ts
@@ -1,22 +1,34 @@
 import { error } from "console";
-import { NextFunction, Router, response, ErrorRequestHandler } from "express";
+import Comment from "../../models/comment";
+import {
+  NextFunction,
+  Express,
+  RequestHandler,
+  Request,
+  Router,
+  response,
+  ErrorRequestHandler,
+} from "express";
 
 const router = Router();
 
 router.post(
-    "/api/comment/:commentid/delete/:postId", async (req: Request, res: Response, next: NextFunction) => {
-        const { postId, commentid } = req.params;
+  "/api/comment/:commentid/delete/:postId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { postId, commentid } = req.params;
 
-        if (!commentid || !postId) {
-            const error = new Error("commentid or postid is missing", 400) as CustomError
-            error.status = 400
-            next(error)
-        }
+    if (!commentid || !postId) {
+      const customError = new Error(
+        "commentid or postid is missing"
+      ) as CustomError;
+      customError.status = 400;
+      return next(customError);
+    }
 
-        try {
-            await commentid.findOneAndDelete(_id: postId)
-        } catch (err) {
-            next(error("Comment cannot be updated."))
-        }
-        
-})
+    try {
+      await Comment.findOneAndDelete({ _id: commentid });
+    } catch (err) {
+      next(new error("Comment cannot be updated."));
+    }
+  }
+);
