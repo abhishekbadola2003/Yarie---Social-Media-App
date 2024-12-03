5dc2bd13efac69347d2feaf84e3aaf5bceeb41c1
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-03 11:55:12 +0530

    Update post.ts

--- src/models/post.ts
+++ src/models/post.ts
@@ -17,6 +17,6 @@ const postSchema = new mongoose.Schema({
   ],
 });
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
 
 export default Post;
