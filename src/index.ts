9889d7024fa3ddcb5eb2c7c1278adf77d9a0941a
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-03 13:38:36 +0530

    Learnings in Typescript and Express.js

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./routers/post/new";
-export * from "./routers/post/delete";
-export * from "./routers/post/update";
-export * from "./routers/post/show";
-
-export * from "./routers/comment/new";
-export * from "./routers/comment/delete";
