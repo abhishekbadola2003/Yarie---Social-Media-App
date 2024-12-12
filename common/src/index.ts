f8784350821aa4eab9b27ac0ae59c009f50ed568
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-12 11:28:09 +0530

    Update index.ts

--- common/src/index.ts
+++ common/src/index.ts
@@ -4,10 +4,11 @@ export * from "./middlewares/current-users";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/req-auth";
 export * from "./middlewares/upload-img";
+export * from "./middlewares/validation-request";
 
-export * from "./errors/bad-request-error";
-export * from "./errors/database-connection-error";
-export * from "./errors/not-authorized";
-export * from "./errors/not-found-error";
-export * from "./errors/custom-errors";
-export * from "./errors/request-validation-error";
+export * from "../src/errors/bad-request-error";
+export * from "../src/errors/database-connection-error";
+export * from "../src/errors/not-authorized";
+export * from "../src/errors/not-found-error";
+export * from "../src/errors/custom-errors";
+export * from "../src/errors/request-validation-error";
